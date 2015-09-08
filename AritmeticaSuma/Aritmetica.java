01735c24ca7d001df8e1f08bb502a78fe7349a6f
Magne H. Carlos F <goomaku@gmail.com>
2015-09-07 23:14:27 -0400

    Suma de dos numeros enteros

---
+++ AritmeticaSuma/Aritmetica.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aritmetica;
+
+/**
+ *
+ * @author 
+ */
+public class Aritmetica {
+    /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+    public static int sumar(int a, int b){
+        return a + b;
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("La suma es: "+ Aritmetica.sumar(5, 2));
+    }
+    
+}
